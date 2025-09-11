b44058643f916fb58fc89947a255a71ae68ae04c
Leandro Pais <leandro.pais@educacionit.com>
2025-09-11 15:26:47 -0300

    Clase 1

---
+++ Clase01/HolaMundo/src/com/techlab/inicio/HolaMundo.java
@@ -0,0 +1,6 @@
+package com.techlab.inicio;
+public class HolaMundo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("¡Hola mundo!");
+    }
+}
---
+++ Clase01/HolaMundoCustom/src/com/techlab/inicio/HolaMundo.java
@@ -0,0 +1,12 @@
+package com.techlab.inicio;
+public class HolaMundo {
+    public static void main(String[] args) throws Exception {
+        
+        boolean msg = Math.random() < 0.5;
+        if(msg)
+            System.out.println("Hola Mundo!");
+        else
+            System.out.println("Hola! Que bueno estar en el curso!!");
+
+    }
+}
